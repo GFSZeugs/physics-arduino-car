8418ba681d08424b5b404e725497ed747b748ee9
tch1b0 <jpour3006@gmail.com>
2024-06-12 11:28:14 +0200

    enhance

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,22 @@ const int dirPin = 5;
 int direction = 0;
 
 void setup() {
-	pinMode(pwmPin, OUTPUT);
-	pinMode(dirPin, OUTPUT);
+	// pinMode(pwmPin, OUTPUT);
+	// pinMode(dirPin, OUTPUT);
 
-	e = new Engine(pwmPin, dirPin);
+	// e = new Engine(pwmPin, dirPin);
 }
 
 void loop() {
-	for (float thrust = 0.1; thrust < 1.0; thrust += 0.1) {
-		(*e).setThrust(thrust);
-		delay(1000);
-	}
+	printf("Hola \n");
+	delay(1000);
+	// for (float thrust = 0.1; thrust < 1.0; thrust += 0.1) {
+	// 	(*e).setThrust(thrust);
+	// 	delay(1000);
+	// }
 
-	digitalWrite(pwmPin, 0);
-	delay(5000);
-	direction = direction == 0 ? 1 : 0;
-	digitalWrite(dirPin, direction);
+	// digitalWrite(pwmPin, 0);
+	// delay(5000);
+	// direction = direction == 0 ? 1 : 0;
+	// digitalWrite(dirPin, direction);
 }
