5ad8ec81dbf03b89278d90d3e2f3dbf46d9eb634
Tch1b0 <Jpour3006@gmail.com>
2024-06-13 19:38:20 +0200

    enhance

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,11 @@ void setup() {
 	// (*e).kickstart(150);
 	// delay(2500);
 	// (*e).setThrust(170);
-	Serial.begin(9600);
 }
 
 void loop() {
-	Serial.println("Hola mi amigo");
-	delay(1000);
-	// analogWrite(PWM_PIN, 200);
-	// delay(10000);
-	// analogWrite(PWM_PIN, 0);
-	// delay(60000);
+	analogWrite(PWM_PIN, 160);
+	delay(10000);
+	analogWrite(PWM_PIN, 0);
+	delay(60000);
 }
