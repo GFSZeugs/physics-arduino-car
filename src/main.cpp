b6c6d041d32bac9ddecab3ddfaf0e84bed58d692
Tch1b0 <Jpour3006@gmail.com>
2024-06-13 18:44:04 +0200

    fix engine

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,16 @@
 #include "engine.h"
 #include "constants.h"
 
-Engine e;
+Engine *e;
 
 void setup() {
 	pinMode(PWM_PIN, OUTPUT);
 	pinMode(DIR_PIN, OUTPUT);
 
-	e = Engine{PWM_PIN, DIR_PIN};
-	e.kickstart(150);
+	e = new Engine{PWM_PIN, DIR_PIN};
+	(*e).kickstart(150);
 	delay(5000);
-	e.setThrust(0);
+	(*e).setThrust(0);
 }
 
 void loop() {
