4c971fdb4aa79053469ae5b119f8e89ca66f1f33
Tch1b0 <Jpour3006@gmail.com>
2024-06-13 19:59:40 +0200

    enhance

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ Engine *e;
 
 void setup() {
 	pinMode(PWM_PIN, OUTPUT);
-	pinMode(DIR_PIN, OUTPUT);
+	// pinMode(DIR_PIN, OUTPUT);
 
-	e = new Engine{PWM_PIN, DIR_PIN};
+	// e = new Engine{PWM_PIN, DIR_PIN};
 	// (*e).kickstart(150);
 	// delay(2500);
 	// (*e).setThrust(170);
