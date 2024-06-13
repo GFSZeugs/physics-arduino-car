b3cdf1020241345f3f2c2edb37245cd3dd98fdff
Tch1b0 <Jpour3006@gmail.com>
2024-06-13 21:04:05 +0200

    enhance

--- src/constants.h
+++ src/constants.h
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int KICKSTART_PWM_MOD = 230;
-const int KICKSTART_DELAY = 500;
+const int KICKSTART_PWM_MOD = 245;
+const int KICKSTART_DELAY = 1000;
 
 const int PWM_PIN = PD5;
 const int DIR_PIN = -1;
--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,18 @@
 #include "engine.h"
 #include "constants.h"
 
-// Engine *e;
+Engine *e;
 
 void setup() {
 	pinMode(PWM_PIN, OUTPUT);
 	// pinMode(DIR_PIN, OUTPUT);
 
-	// e = new Engine{PWM_PIN, DIR_PIN};
-	// (*e).kickstart(150);
-	// delay(2500);
-	// (*e).setThrust(170);
-	Serial.begin(9600);
+	e = new Engine{PWM_PIN, DIR_PIN};
 	delay(5000);
+	(*e).kickstart(220);
+	delay(10000);
+	(*e).setThrust(0);
 }
 
 void loop() {
-	for (int i = 150; i < 250; i++) {
-		analogWrite(PWM_PIN, i);
-		Serial.println(i);
-		delay(500);
-	}
-
-
-	analogWrite(PWM_PIN, 0);
-	delay(60000);
 }
